660377aca2bce55bc4a8082d9551d8665bd1e596
Brandon Aaronson <45774261+brandonaaronson@users.noreply.github.com>
2020-11-08 13:55:13 -0800

    forgot a save

--- src/app/service/search.service.ts
+++ src/app/service/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Missionary } from '../models/missionary';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
